3c83b18cf8b7bab6f05724a3d0b0b7881a2a4bd8
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-12-11 14:57:24 -0800

    feat: vault 1.14.6 -> 1.14.8

--- vault-backup.sh
+++ vault-backup.sh
@@ -33,7 +33,7 @@ BASE_JSON='{
     "source_backup_url": "'"$BACKUP_S3_PRESIGNED_URL"'",
     "source_keys_url": "'"$TOKENS_S3_PRESIGNED_URL"'",
     "path_values_map":{},
-    "vault_version": "1.14.6"
+    "vault_version": "1.14.8"
 }'
 UPDATED_JSON=$(echo $BASE_JSON | jq --arg path "secret/$FIRST_SECRET" --argjson kv "$KEY_VALUES" '.path_values_map[$path] = $kv')
 echo "Validating Backup now....."
