a714c6945d43b610c5f2aa26f5548250bfc7bb55
Venkat <venkata@venkatamutyala.com>
2024-10-14 07:31:48 +0000

    feat: parameterize the ref name and add some checks before running the curl

--- github-workflow-dispatcher.sh
+++ github-workflow-dispatcher.sh
@@ -2,10 +2,22 @@
 set -e
 
 
+
+# Check if required variables are set
+if [ -z "${GITHUB_DISPATCH_URL}" ]; then
+    echo "Error: GITHUB_DISPATCH_URL is not set."
+    exit 1
+fi
+
+if [ -z "${REF_NAME}" ]; then
+    echo "Error: REF_NAME is not set."
+    exit 1
+fi
+
 curl \
   --fail \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
   -H "Authorization: token ${GITHUB_TOKEN}" \
   ${GITHUB_DISPATCH_URL} \
-  -d '{"ref":"refs/heads/main"}'
+  -d "{\"ref\":\"${REF_NAME}\"}" 
