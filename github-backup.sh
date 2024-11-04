bf898365b887bc29cb1ee8aa78af97fb0e3dcfc8
Venkat <venkata@venkatamutyala.com>
2024-11-04 15:23:39 +0000

    feat: update github backup script to do all orgs that the user has access too

--- github-backup.sh
+++ github-backup.sh
@@ -2,29 +2,56 @@
 
 set -e
 
-# Check if required variables are set
-if [[ -z "${GITHUB_ORG_TO_BACKUP}" ]]; then
-    echo "Error: GITHUB_ORG_TO_BACKUP is not set."
-    exit 1
-fi
-
 if [[ -z "${GITHUB_TOKEN}" ]]; then
     echo "Error: GITHUB_TOKEN is not set."
     exit 1
 fi
 
-repos=$(gh repo list $GITHUB_ORG_TO_BACKUP -L 100000 --json nameWithOwner -q '.[].nameWithOwner')
 
-BACKUP_DATE=$(date '+%Y-%m-%d')
-BACKUP_LOCATION="github.com/$BACKUP_DATE/$GITHUB_ORG_TO_BACKUP"
-mkdir -p $BACKUP_LOCATION && cd $BACKUP_LOCATION
+# Initialize page number
+page=1
+all_orgs=""
 
-for repo in $repos; do
-    git clone --mirror https://$GITHUB_TOKEN@github.com/$repo.git
-    repo_name="${repo##*/}"
-    tar -czf "${repo_name}.tar.gz" "${repo_name}.git" && rm -rf "${repo_name}.git"
+# Loop until we get an empty response
+while true; do
+    # Fetch organizations for the current page
+    orgs=$(gh api "/user/orgs?page=$page" --jq '.[].login')
+
+    # Check if the response is empty, meaning no more orgs
+    if [ -z "$orgs" ]; then
+        break
+    fi
+
+    # Append organizations to the all_orgs variable
+    all_orgs="$all_orgs\n$orgs"
+
+    # Increment the page number
+    page=$((page + 1))
 done
 
-echo "Uploading everything to S3...."
-cd /app
-aws s3 cp --recursive github.com/ s3://${S3_BUCKET_NAME}/github.com/
+
+echo "Full list to be backed up: $orgs"
+
+echo "$all_orgs" | while IFS= read -r GITHUB_ORG_TO_BACKUP; do
+
+        echo "STARTING BACKUP OF: https://github.com/${GITHUB_ORG_TO_BACKUP}"
+
+        repos=$(gh repo list $GITHUB_ORG_TO_BACKUP -L 100000 --json nameWithOwner -q '.[].nameWithOwner')
+
+        BACKUP_DATE=$(date '+%Y-%m-%d')
+        BACKUP_LOCATION="github.com/$BACKUP_DATE/$GITHUB_ORG_TO_BACKUP"
+        mkdir -p $BACKUP_LOCATION && cd $BACKUP_LOCATION
+
+        for repo in $repos; do
+            git clone --mirror https://$GITHUB_TOKEN@github.com/$repo.git
+            repo_name="${repo##*/}"
+            tar -czf "${repo_name}.tar.gz" "${repo_name}.git" && rm -rf "${repo_name}.git"
+        done
+
+        echo "Uploading everything to S3...."
+        cd /app
+        aws s3 cp --recursive github.com/ s3://${S3_BUCKET_NAME}/github.com/
+
+
+        echo "FINISHED BACKUP OF: https://github.com/${GITHUB_ORG_TO_BACKUP}"
+done
